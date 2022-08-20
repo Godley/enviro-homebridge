0eeaf509e8bc152afd3229a1e5ef7b532913df4e
Charlotte <charlotte@godley.dev>
2022-08-20 23:20:34 +0100

    fix name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'PimoroniEnviroPlugin';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'pimoroni-enviro-plugin';
+export const PLUGIN_NAME = 'homebridge-pimoroni-enviro';
